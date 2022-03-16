cea2ce08094489a06b619f2c143b0d7d6fbf42c4
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2022-03-16 10:27:21 +0000

    Update asliases with cuv

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,7 @@ alias o='open'
 alias gs="git status"
 alias gl="git log"
 alias gca="git commit -am"
+alias gcamne='git commit --amend -a --no-edit'
 
 #bundle
 alias bef='bundle exec fastlane'
@@ -14,3 +15,9 @@ alias be='bundle exec'
 
 alias pi='pod install'
 alias dot='cd $HOME/Documents/dotfiles && l'
+
+# cuvva
+alias cuv0='cd /Users/anton/Documents/iOS/app-cuvva-ios'
+alias cuv1='git checkout develop && git pull origin develop'
+alias cuv2='git checkout -'
+alias cuv3='git merge develop'
